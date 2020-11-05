21fc5d79c7448a0b37013fc319fbb0450168b6ca
bodroghykristof <bodroghykristof@gmail.com>
2020-11-05 21:08:03 +0100

    Read data from config.properties file instead of environment variables

--- src/main/java/com/codecool/emailspammer/app/App.java
+++ src/main/java/com/codecool/emailspammer/app/App.java
@@ -5,19 +5,21 @@ import com.codecool.emailspammer.io.Logger;
 import com.codecool.emailspammer.mails.Mail;
 import com.codecool.emailspammer.mails.MailBox;
 import com.codecool.emailspammer.mails.Postman;
+import com.codecool.emailspammer.network.EmailPropertyValues;
 import com.codecool.emailspammer.network.MailHandler;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.io.IOException;
+
 
 public class App {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
 
         Logger logger = new ConsoleLogger();
         logger.printTitle("Welcome To Email Spammer");
-        MailHandler mailHandler = new MailHandler(System.getenv("EMAIL"), System.getenv("EMAIL_PASSWORD"));
+
+        EmailPropertyValues values = new EmailPropertyValues();
+        MailHandler mailHandler = new MailHandler(values.getEmail(), values.getPassword());
         MailBox mailBox = new MailBox();
         Postman postman = new Postman(mailBox, mailHandler);
         Thread delivering = new Thread(postman);
