e9318f882cb8fbcabde653faa526ed452410b6d6
bodroghykristof <bodroghykristof@gmail.com>
2020-11-05 13:52:13 +0100

    Implement sending emails on multiple threads continuously

--- src/main/java/com/codecool/emailspammer/app/App.java
+++ src/main/java/com/codecool/emailspammer/app/App.java
@@ -4,22 +4,29 @@ import com.codecool.emailspammer.io.ConsoleLogger;
 import com.codecool.emailspammer.io.Logger;
 import com.codecool.emailspammer.network.MailHandler;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         Logger logger = new ConsoleLogger();
         logger.printTitle("Welcome To Email Spammer");
         String clientEmail = logger.askEmailAddress();
         MailHandler mailHandler = new MailHandler(clientEmail);
+        ExecutorService executor = Executors.newCachedThreadPool();
 
         while (true) {
 
             String message = logger.askMessage();
             if (message.toLowerCase().equals("quit")) break;
-            mailHandler.sendMail(message);
-
+            executor.submit(() -> mailHandler.sendMail(message));
         }
+
+        executor.shutdown();
+        executor.awaitTermination(3, TimeUnit.SECONDS);
 
     }
 
